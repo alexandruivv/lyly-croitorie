28d9b001a62b2bf1f33de5dc791e95d0158ebd93
leoivv <leonardivv39@gmail.com>
2025-02-09 20:54:48 +0200

    Programari fix

--- src/app/components/ImageText.tsx
+++ src/app/components/ImageText.tsx
@@ -1,21 +1,49 @@
-import React from 'react';
-import Image, {StaticImageData} from "next/image";
+import React from "react";
+import Image, { StaticImageData } from "next/image";
 
 interface Props {
-    text: string;
-    classNameContainer?: string;
-    classNameImg?: string;
-    classNameParagraph?: string;
-    img: StaticImageData;
+  text: string;
+  classNameContainer?: string;
+  classNameImg?: string;
+  classNameParagraph?: string;
+  img: StaticImageData;
+  href?: string;
+  onClick?: () => void;
 }
 
-const ImageText = ({text, classNameContainer, classNameImg, classNameParagraph, img}: Props) => {
+const ImageText = ({
+  text,
+  classNameContainer,
+  classNameImg,
+  classNameParagraph,
+  img,
+  href,
+  onClick,
+}: Props) => {
+  const content = (
+    <>
+      <Image src={img} alt="check-icon" className={classNameImg} />
+      <span className={`font-semibold break-all ${classNameParagraph}`}>
+        {text}
+      </span>
+    </>
+  );
+
+  const containerClass = `flex flex-row gap-2 items-center ${classNameContainer} ${
+    href || onClick
+      ? "hover:text-brownHover transition-colors duration-300 cursor-pointer"
+      : ""
+  }`;
+
+  if (href || onClick) {
     return (
-        <div className={`flex flex-row gap-2 items-center ${classNameContainer}`}>
-            <Image src={img} alt="check-icon" className={classNameImg}/>
-            <p className={`font-semibold break-all ${classNameParagraph}`}>{text}</p>
-        </div>
+      <a href={href} onClick={onClick} className={containerClass}>
+        {content}
+      </a>
     );
+  }
+
+  return <div className={containerClass}>{content}</div>;
 };
 
 export default ImageText;
--- src/app/pages/Programari.tsx
+++ src/app/pages/Programari.tsx
@@ -1,70 +1,109 @@
 "use client";
 
-import GoArrow, {GoArrowDirection} from "../components/GoArrow";
+import GoArrow, { GoArrowDirection } from "../components/GoArrow";
 import Button from "../components/Button";
 import ImageText from "@/app/components/ImageText";
-import clockImg from '../../../public/images/clock.png';
-import fbImg from '../.././../public/images/Facebook.png';
-import mailImg from '../../../public/images/mail.png';
-import phoneImg from '../../../public/images/phone.png';
-import FormField, {FormFieldType} from "@/app/components/FormField";
-import {scrollToSection} from "@/app/utils/utils";
+import clockImg from "../.././../public/images/Clock.png";
+import fbImg from "../.././../public/images/Facebook.png";
+import mailImg from "../.././../public/images/Mail.png";
+import phoneImg from "../.././../public/images/Phone.png";
+import FormField, { FormFieldType } from "@/app/components/FormField";
+import { scrollToSection } from "@/app/utils/utils";
 import bgMobileImg from "../../../public/images/despre_ce_e_vorba_bg_mobile.webp";
 import bgImg from "../../../public/images/despre_ce_e_vorba_bg.webp";
-import {CustomCSSProperties} from "@/app/types/CustomCSSProperties";
+import { CustomCSSProperties } from "@/app/types/CustomCSSProperties";
 
 const Programari = () => {
-    return (
-        <div
-            id="programari"
-            style={{'--mobile-image-url': `url(${bgMobileImg.src})`, '--image-url': `url(${bgImg.src})`} as CustomCSSProperties}
-            className="relative w-full h-screen bg-cover bg-center bg-no-repeat bg-[image:var(--mobile-image-url)] lg:bg-[image:var(--image-url)]"
-        >
-            <div className="absolute inset-0 bg-white bg-opacity-80"></div>
+  return (
+    <div
+      id="programari"
+      style={
+        {
+          "--mobile-image-url": `url(${bgMobileImg.src})`,
+          "--image-url": `url(${bgImg.src})`,
+        } as CustomCSSProperties
+      }
+      className="relative w-full h-screen bg-cover bg-center bg-no-repeat bg-[image:var(--mobile-image-url)] lg:bg-[image:var(--image-url)]"
+    >
+      <div className="absolute inset-0 bg-white bg-opacity-80"></div>
 
-            <div className="relative h-full flex flex-col py-24 px-5 lg:py-32 text-black justify-center">
-                <div className='flex flex-col md:gap-10 mx-auto'>
-                    <h1 className="font-bold text-2xl md:text-5xl text-center max-lg:mt-5 lg:my-10">
-                        Programari
-                    </h1>
-                    <div
-                        className='grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-4 mx-auto w-full p-2 lg:p-10 max-lg:gap-y-5'>
-                        <ImageText classNameImg='max-md:w-5 max-lg:w-8'
-                                   classNameParagraph='text-sm lg:text-xl'
-                                   text='0742 153 656'
-                                   img={phoneImg}/>
-                        <ImageText
-                            classNameImg='max-md:w-5 max-lg:w-8'
-                            classNameParagraph='text-sm lg:text-xl'
-                            text='contact@lylycroitorie.ro'
-                            img={mailImg}/>
-                        <ImageText
-                            classNameImg='max-md:w-5 max-lg:w-8'
-                            classNameParagraph='text-sm lg:text-xl'
-                            text='Lu-Du 7:00-18:00'
-                            img={clockImg}/>
-                        <ImageText classNameImg='max-md:w-5 max-lg:w-8'
-                                   classNameParagraph='text-sm lg:text-xl'
-                                   text='facebook.com/lylycroitorie'
-                                   img={fbImg}/>
-                    </div>
-                </div>
+      <div className="relative h-full flex flex-col justify-between pt-24 pb-16 md:pt-28 md:pb-8 text-black">
+        <h1 className="font-bold text-2xl md:text-5xl text-center">
+          Programări
+        </h1>
 
-                <form className='flex flex-col gap-2 lg:px-10'>
-                    <FormField label='Nume' type={FormFieldType.TEXT} id='nume' placeholder='Introduceti numele...'/>
-                    <FormField label='Telefon' type={FormFieldType.TEXT} id='telefon'
-                               placeholder='Introduceti telefonul...'/>
-                    <FormField label='Email' type={FormFieldType.TEXT} id='email' placeholder='Introduceti emailul...'/>
-                    <FormField label='Mesaj' type={FormFieldType.TEXTAREA} id='mesaj'
-                               placeholder='Introduceti mesajul...'/>
-                    <Button text="Programează acum!" className="mt-2 mx-auto"/>
-                </form>
-                <GoArrow className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4"
-                         direction={GoArrowDirection.UP}
-                         onClick={() => scrollToSection('acasa')}/>
+        <div className="flex-1 flex flex-col justify-center gap-4 mt-2">
+          <div className="w-fit mx-auto">
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-3 p-2 lg:p-6 max-lg:gap-y-3">
+              <ImageText
+                classNameImg="max-md:w-4 max-lg:w-6"
+                classNameParagraph="text-xs lg:text-lg"
+                text="0742 153 656"
+                img={phoneImg}
+                href="tel:0742153656"
+              />
+              <ImageText
+                classNameImg="max-md:w-4 max-lg:w-6"
+                classNameParagraph="text-xs lg:text-lg"
+                text="contact@lylycroitorie.ro"
+                img={mailImg}
+                href="mailto:contact@lylycroitorie.ro"
+              />
+              <ImageText
+                classNameImg="max-md:w-4 max-lg:w-6"
+                classNameParagraph="text-xs lg:text-lg"
+                text="Lu-Du 7:00-18:00"
+                img={clockImg}
+              />
+              <ImageText
+                classNameImg="max-md:w-4 max-lg:w-6"
+                classNameParagraph="text-xs lg:text-lg"
+                text="facebook.com/lylycroitorie"
+                img={fbImg}
+                href="https://facebook.com/lylycroitorie"
+              />
             </div>
+          </div>
+
+          <form className="flex flex-col gap-1.5 lg:px-8">
+            <FormField
+              label="Nume"
+              type={FormFieldType.TEXT}
+              id="nume"
+              placeholder="Introduceti numele..."
+            />
+            <FormField
+              label="Telefon"
+              type={FormFieldType.TEXT}
+              id="telefon"
+              placeholder="Introduceti telefonul..."
+            />
+            <FormField
+              label="Email"
+              type={FormFieldType.TEXT}
+              id="email"
+              placeholder="Introduceti emailul..."
+            />
+            <FormField
+              label="Mesaj"
+              type={FormFieldType.TEXTAREA}
+              id="mesaj"
+              placeholder="Introduceti mesajul..."
+            />
+            <Button text="Programează acum!" className="mt-3 mx-auto" />
+          </form>
         </div>
-    );
+
+        <div className="relative h-12 w-full">
+          <GoArrow
+            className="absolute left-1/2 transform -translate-x-1/2 top-0"
+            direction={GoArrowDirection.UP}
+            onClick={() => scrollToSection("acasa")}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Programari;
