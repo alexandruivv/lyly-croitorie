b17ef9fdfaa4b9249c1e1c09ffbca4466430292a
ecommadv <140523675+ecommadv@users.noreply.github.com>
2025-02-15 15:53:06 +0200

    icon size

--- src/app/components/CumProcedamDescriere.tsx
+++ src/app/components/CumProcedamDescriere.tsx
@@ -11,7 +11,7 @@ const CumProcedamDescriere = ({ numar, icon, descriere }: Descriere) => {
   return (
     <div className="flex flex-col gap-4 md:gap-10 justify-center align-middle text-center border relative bg-peach shadow-lg p-4 md:p-8">
       <span className="absolute top-2 left-5 md:top-5 md:left-5 text-2xl md:text-3xl lg:text-8xl">{numar}</span>
-      <Image src={icon} alt={"icon" + numar} className="mx-auto max-sm:h-5 max-sm:w-5 max-md:h-14 max-md:w-14" />
+      <Image src={icon} alt={"icon" + numar} className="mx-auto max-sm:h-12 max-sm:w-12 max-md:h-14 max-md:w-14" />
       <p className="max-sm:text-xs">{descriere}</p>
     </div>
   );
