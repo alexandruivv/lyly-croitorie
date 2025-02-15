184b47ac8c4567e2de3e9e9172c74cef1b03417d
ecommadv <140523675+ecommadv@users.noreply.github.com>
2025-02-15 17:02:39 +0200

    fixes

--- src/app/pages/DespreNoi.tsx
+++ src/app/pages/DespreNoi.tsx
@@ -109,12 +109,12 @@ const Servicii = () => {
           <Image
             src={despreNoiImg}
             alt="despre-noi-image"
-            className="rounded-xl lg:w-4/12"
+            className="rounded-xl lg:w-2/12"
           />
           <Image
             src={despreNoiImg2}
             alt="despre-noi-image2"
-            className="rounded-xl  lg:w-4/12"
+            className="rounded-xl  lg:w-2/12"
           />
         </div>
 
