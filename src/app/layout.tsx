34eb8c72438d057b452c634eff5c87d9e3bacea6
ecommadv <140523675+ecommadv@users.noreply.github.com>
2025-02-16 17:54:05 +0200

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import { Poppins } from "next/font/google";
-import { ToastContainer } from "react-toastify";
+import {Poppins} from "next/font/google";
+import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Head from "next/head";
+import {Metadata} from "next";
 
+export const metadata: Metadata = {
+  title: 'Lyly - Croitorie Mobila La Comanda!',
+  description:
+    'lylydesign.ro: Croitorie mobilă la comandă. Retușuri, ajustări și creații vestimentare personalizate, direct la tine acasă. Calitate și eleganță la fiecare cusătură!',
+  keywords: 'croitorie mobilă, Lyly Design, retușuri haine, ajustări vestimentare, croitor la domiciliu, croitorie personalizată, modificări haine, reparații vestimentare, servicii croitorie, croitor profesionist, croitorie la comandă, cusături de calitate, design vestimentar, haine la comandă'
+};
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -12,19 +18,19 @@ const poppins = Poppins({
 });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} font-medium`}>
-        <div className="main-container">
-          <Navbar />
-          <main>{children}</main>
-        </div>
-        <ToastContainer />
-      </body>
+    <body className={`${poppins.className} font-medium`}>
+    <div className="main-container">
+      <Navbar/>
+      <main>{children}</main>
+    </div>
+    <ToastContainer/>
+    </body>
     </html>
   );
 }
