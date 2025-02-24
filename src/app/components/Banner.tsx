eda9a59747e1f46afa648ab18d144e195f3dfddf
alexandruivanov <alexandru.ivanov98@gmail.com>
2025-02-25 01:26:24 +0200

    improvement

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className='fixed top-0 left-0 h-10 bg-black w-full text-white text-xs z-50 flex flex-col lg:flex-row lg:gap-4 items-center justify-center'>
+    <div className='fixed top-0 left-0 h-10 bg-black w-full text-white text-xs z-50 flex flex-col lg:flex-row lg:gap-1 items-center justify-center'>
         <p>🎁 Prinde oferta doar azi de -10% </p>
-        <p>⏱️ Retușuri în 24h 🚗 Acoperim Piatra-Neamț (+10 km)</p>
+        <p>⏱️ Retușuri în 24h  🚗 Acoperim Piatra-Neamț (+10 km)</p>
     </div>
   );
 };
